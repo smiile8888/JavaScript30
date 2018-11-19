5f65d63e31d4e62e57735224865fbffdf5dc2401
smiile8888 <th.thanyathorn@gmail.com>
2018-11-19 18:49:43 -0500

    Day 2 - Completed!

---
+++ 02 - CSS + JS Clock/README.md
@@ -0,0 +1,10 @@
+# Day 2: CSS + JS Clock
+Today, the practice is about creating a clock by using CSS for animation and JS for collecting and updating data behind the scence.
+
+Today I'm getting to know:
+In CSS:
+- `transform` - uses to rotate/transform the elements, in this case are the clock's hands.
+- `transform-origin` - allows to change the position of transformed elements.
+
+In JS:
+- `Date()` - gets the current date time, which allows to get details in hours, minutes and seconds.
---
+++ 02 - CSS + JS Clock/clock.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    const secondHand = document.querySelector('.second-hand');
+    const minHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    function setDate() {
+        const now = new Date();
+        
+        const seconds = now.getSeconds();
+        const secondsDegree = ((seconds / 60) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+        const mins = now.getMinutes();
+        const minsDegree = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+        minHand.style.transform = `rotate(${minsDegree}deg)`;
+
+        const hours = now.getHours();
+        const hoursDegree = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+        hourHand.style.transform = `rotate(${hoursDegree}deg)`
+    }
+
+    // call setDate() every second
+    setInterval(setDate, 1000);
+}
